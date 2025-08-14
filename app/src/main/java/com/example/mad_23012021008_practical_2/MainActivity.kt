43603286faeb328e39cf62722d851eb35e3014e9
Shruti Diwani <cb.diwani214@gmail.com>
2025-08-14 13:18:45 +0530

    kjk

---
+++ app/src/main/java/com/example/mad_23012021008_practical_2/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.mad_23012021008_practical_2
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivty"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        showMessage("onCreate method called")
+    }
+    fun showMessage(msg:String){
+        Log.i(TAG, "showMessage: $msg")
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+    override fun onStart() {
+        showMessage("onStart method called")
+        super.onStart()
+    }
+    override fun onResume() {
+        showMessage("onResume method called")
+        super.onResume()
+    }
+    override fun onPause() {
+        showMessage("onPause method called")
+        super.onPause()
+    }
+    override fun onStop() {
+        showMessage("onStop method called")
+        super.onStop()
+    }
+    override fun onDestroy() {
+        showMessage("onDeatroy method called")
+        super.onDestroy()
+    }
+    override fun onRestart() {
+        showMessage("onRestart method called")
+        super.onRestart()
+    }
+}
